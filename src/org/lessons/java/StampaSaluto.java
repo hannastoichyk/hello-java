a1fcb94c506682a9b1025c52feba80748398f51c
Hanna Stoichyk <hannastoichyk@gmail.com>
2023-11-28 15:31:28 +0100

    firstcommit

---
+++ src/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,7 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+    public static void main(String[] args) {
+        //qui è dove scrivo il mio codice eseguibile
+        System.out.println("Hello world!");}
+}
